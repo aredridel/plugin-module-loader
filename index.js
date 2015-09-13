ed5d7de1b89314f1af53cafb977c5a4b44d80e7d
Aria Stewart <aredridel@dinhe.net>
2015-09-12 21:48:48 -0400

    deps

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var glob = P.promisify(require('glob'));
+var fs = P.promisifyAll(require('fs'));
+var VError = require('verror');
+var P = require('bluebird');
+
 function loadPluginsAndCollectErrors(pluginDir) {
   return glob(path.resolve(pluginDir, 'lib/node_modules/*/package.json')).then(function(plugins) {
     return plugins.map(function(plugin) {
